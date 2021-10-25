e8b20a115d08fe8b8eae0df42b12692613eb025c
Vassysuary <92851690+Vassysuary@users.noreply.github.com>
2021-10-26 00:40:22 +1000

    Revert "Homework1"

--- src/main/java/ru/geekbrains/jca/lessons/l1_intro/HelloWorld.java
+++ src/main/java/ru/geekbrains/jca/lessons/l1_intro/HelloWorld.java
@@ -3,10 +3,6 @@ package ru.geekbrains.jca.lessons.l1_intro;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
     }
 
 }
